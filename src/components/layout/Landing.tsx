b638f710ffdd67802c6ae46408d7e3059e1f0cc3
myc37 <nvjn37@gmail.com>
2023-03-11 16:20:41 +0800

    feat: add graphic to landing page

--- src/components/layout/Landing.tsx
+++ src/components/layout/Landing.tsx
@@ -7,12 +7,22 @@ type Props = {
 
 const Landing: FC<Props> = ({ datePickerProps }) => {
 	return (
-		<div className="flex flex-col justify-center ml-4 h-screen">
-			<div className="text-xl md:text-3xl mb-8 font-semibold">
-				Making traffic camera images more accesible to all in Singapore
+		<div className="flex flex-col md:flex-row justify-center gap-y-8 md:gap-x-16 h-screen md:h-auto">
+			<div className="flex flex-col justify-center ml-4 md:w-1/3 md:h-screen">
+				<div className="text-xl md:text-3xl mb-8 font-semibold">
+					Making traffic camera images more accesible to all in
+					Singapore
+				</div>
+				<div className="italic">
+					Select a date and time to get started
+				</div>
+				<DatePicker {...datePickerProps} />
 			</div>
-			<div className="italic">Select a date and time to get started</div>
-			<DatePicker {...datePickerProps} />
+			<img
+				src="landing.jpeg"
+				alt="landing"
+				className="md:mt-0 object-contain p-8"
+			/>
 		</div>
 	);
 };
