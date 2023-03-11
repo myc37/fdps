83e9f12ae0bc12ba6b32a91cf140b859b5cc6282
myc37 <nvjn37@gmail.com>
2023-03-12 05:12:02 +0800

    fix: update grid component due to type change

--- src/components/TrafficImagesGrid.tsx
+++ src/components/TrafficImagesGrid.tsx
@@ -10,9 +10,9 @@ export const TrafficImagesGrid: FC<Props> = ({ display }) => {
 	return (
 		<Fade cascade triggerOnce>
 			<div className="p-8 grid grid-cols-1 md:grid-cols-4 gap-y-4 md:gap-4 w-full">
-				{display.map((doc) => (
+				{display.map((doc, idx) => (
 					<div
-						key={doc.image.id}
+						key={`grid-item-${idx}`}
 						className="bg-neutral-50 rounded-md border-2 p-2 flex flex-col drop-shadow-md transition-colors hover:border-green-600"
 					>
 						<div className="text-sm mb-1 mx-2 capitalize flex justify-between">
@@ -22,7 +22,7 @@ export const TrafficImagesGrid: FC<Props> = ({ display }) => {
 							<div>{doc.weather}</div>
 						</div>
 						<img
-							src={doc.image.src}
+							src={doc.image}
 							alt={doc.streetName}
 							style={{ aspectRatio: "16 / 9" }}
 							className="rounded-md m-2"
