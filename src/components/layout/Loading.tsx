f325d2ea6520cba33f72ce774c8995d4e2f0226f
myc37 <nvjn37@gmail.com>
2023-03-11 16:22:21 +0800

    style: update loading screen height

--- src/components/layout/Loading.tsx
+++ src/components/layout/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
 	return (
-		<div className="flex-grow w-full flex flex-col justify-center items-center">
+		<div className="h-screen w-full flex flex-col justify-center items-center">
 			<div className="lds-spinner">
 				{Array.from(Array(12).keys()).map((idx) => (
 					<div key={idx} />
